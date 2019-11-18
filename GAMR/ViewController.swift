8e18c4c686db4a21097656cf6a849e1546644bc8
Rayan Syed <rayansyed41@gmail.com>
2019-11-18 13:11:46 -0500

    Initial Commit

---
+++ GAMR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GAMR
+//
+//  Created by Rayan Syed on 2019-11-18.
+//  Copyright © 2019 Rayan Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
