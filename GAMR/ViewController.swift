1a3832b84c83ff981ce7047cd6594e6a64f307cd
Rayan Syed <rayansyed41@gmail.com>
2019-11-18 13:55:45 -0500

    Firebase Setup

--- GAMR/ViewController.swift
+++ GAMR/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let ref = Database.database().reference()
+        //ref.child("wahby/name").setValue("Wahab Shah")
+        //ref.childByAutoId().setValue(["name":"Bill"])
     }
 
 
