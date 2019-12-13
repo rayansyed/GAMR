446f95074a6eba72f516274f78a3338a7249bceb
Wahab <wahab.ayub.shah@gmail.com>
2019-12-13 16:39:38 -0500

    App manual with call and email service home page quotes json parsing

--- GAMR/AppManualVC.swift
+++ GAMR/AppManualVC.swift
@@ -7,24 +7,65 @@
 //
 
 import UIKit
+import MessageUI
 
-class AppManualVC: UIViewController {
+class AppManualVC: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        override func viewDidLoad()
+        {
+            super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+            
+        }
+        
+    
+    @IBAction func callMeButton(_ sender: UIButton)
+    {
+        let url:NSURL = URL(string: "TEL://6478662821")! as NSURL
+        UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+    }
+        
+        
+        @IBAction func emailMeButton(_ sender: UIButton)
+        {
+            if MFMailComposeViewController.canSendMail()
+            {
+                let mail = MFMailComposeViewController()
+                mail.mailComposeDelegate = self as! MFMailComposeViewControllerDelegate
+                mail.setToRecipients(["support@gamr.com"])
+                present(mail, animated: true)
+                            
+            }
+            else
+            {
+                print("cannot send email")
+            }
+            
+            func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?)
+            {
+                controller.dismiss(animated: true)
+            }
+            
+        
+        }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+    AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
     }
     
 
-    /*
-    // MARK: - Navigation
+        
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        /*
+        // MARK: - Navigation
+
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
 
 }
